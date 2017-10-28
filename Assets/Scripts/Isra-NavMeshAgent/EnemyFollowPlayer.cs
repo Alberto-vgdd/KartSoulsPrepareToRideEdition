313a26a909d49b40c866a8e26cc1ea6548089366
Israel Nebot <israel.nebot@gmail.com>
2017-10-28 23:59:02 +0200

    Finished creating Nav Mesh Enemy

--- Assets/Scripts/Isra-NavMeshAgent/EnemyFollowPlayer.cs
+++ Assets/Scripts/Isra-NavMeshAgent/EnemyFollowPlayer.cs
@@ -6,16 +6,29 @@ using UnityEngine.AI;
 public class EnemyFollowPlayer : MonoBehaviour {
 
 	NavMeshAgent m_NavMeshAgent;
+	GameObject m_Player;
 	Vector3 m_PlayerPosition;
+
+
 	// Use this for initialization
 	void Start () {
-		m_PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+		m_Player = GameObject.Find("Player");
 		m_NavMeshAgent = GetComponent<NavMeshAgent>();
-		m_NavMeshAgent.SetDestination(m_PlayerPosition);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if(m_Player.GetComponent<MensajesHUDJugador>().invadido){
+			Debug.Log("a");
+			m_PlayerPosition = m_Player.transform.position;
+			transform.LookAt(m_PlayerPosition);
+			m_NavMeshAgent.SetDestination(m_PlayerPosition);
+			m_NavMeshAgent.Resume();
+			if(Vector3.Distance(transform.position, m_PlayerPosition)< 2){
+				m_Player.GetComponent<Rigidbody>().AddForce(transform.forward * 50000, ForceMode.Acceleration);
+			}
+
+		}
 	}
 }
