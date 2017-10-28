bc321519e41f457d211fa3ffa523738294fe4992
Israel Nebot <israel.nebot@gmail.com>
2017-10-28 23:18:35 +0200

    Nav mesh code created

---
+++ Assets/Scripts/Isra-NavMeshAgent/EnemyFollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollowPlayer : MonoBehaviour {
+
+	NavMeshAgent m_NavMeshAgent;
+	Vector3 m_PlayerPosition;
+	// Use this for initialization
+	void Start () {
+		m_PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+		m_NavMeshAgent = GetComponent<NavMeshAgent>();
+		m_NavMeshAgent.SetDestination(m_PlayerPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
