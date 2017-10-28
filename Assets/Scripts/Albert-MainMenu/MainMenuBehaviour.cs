5e1d56f1b17d1ae73cf7e3bec224a1ed5161f1eb
Alberto-vgdd <alberto.vgdd@gmail.com>
2017-10-28 18:06:24 +0200

    AnimacionMenuPrincipal
    POR FIN (falta asignar al script del menu la escena a cargar cuando taca
    y pulsar boton de iniciar partida)

---
+++ Assets/Scripts/Albert-MainMenu/MainMenuBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuBehaviour : MonoBehaviour {
+
+	public GameObject LogoCanvas;
+	public GameObject playButton;
+	public GameObject QuitButton;
+	public GameObject gameTitle;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.GetComponent<Animation>().Play();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+			
+	}
+
+	public void StartGame()
+	{
+		//SceneManager.LoadScene("");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+
+}
