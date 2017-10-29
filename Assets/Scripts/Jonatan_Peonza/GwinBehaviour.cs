bcb46c363ebeedebb71c3464b39d35c5a31e4f50
Jonatan <al315616@uji.es>
2017-10-29 11:55:44 +0100

    Jonatan_Peonza Lit
    +Gwin ahora te quita vida, y te empuja, además se ha ajustado el mapa al
    jugador

---
+++ Assets/Scripts/Jonatan_Peonza/GwinBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GwinBehaviour : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerMovementScript>().ApplyLife(-20);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(this.transform.forward * 1000f,
+                                                                 ForceMode.VelocityChange);
+            
+        }
+    }
+}
