54deb1837562eb9829a78911790b4ea79d05a5ec
AlbertDols <al315151@uji.es>
2017-10-29 15:37:47 +0100

    GraveyardLevel
    Pues eso, FALTA PONERLE LO DE LOS HUESOS.

---
+++ Assets/Scripts/Albert-Levels/GraveyardTriggers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveyardTriggers : MonoBehaviour {
+
+
+	public int triggerID;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (triggerID == 1 && other.tag == "Player")
+		{
+
+		}
+		if (triggerID == 2 && other.tag == "Player")
+		{
+			
+		}
+		if (triggerID == 3 && other.tag == "Player")
+		{
+			
+		}
+	}
+
+
+}
