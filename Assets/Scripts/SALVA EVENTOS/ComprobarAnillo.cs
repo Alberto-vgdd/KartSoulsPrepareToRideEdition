af8390362e443df1a8d307857cc617ed3bbf7bef
Slava <superm30@hotmail.com>
2017-10-29 14:46:33 +0100

    Revert "Revert "Merge pull request #71 from Alberto-vgdd/SALVA""
    This reverts commit 537f33a0b733837957d8efe4edc4a6201ea6be54.

---
+++ Assets/Scripts/SALVA EVENTOS/ComprobarAnillo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComprobarAnillo : MonoBehaviour {
+
+
+	public GameObject anillo;
+
+	// Update is called once per frame
+	void Update () {
+		if (anillo == null) {
+			Destroy (this);
+		}
+	}
+}
