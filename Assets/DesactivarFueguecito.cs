a85c48f25f495f3c25413203a2a90e5dff38e3c4
Alberto <alberto.vgdd@gmail.com>
2017-10-29 13:15:58 +0100

    eifwjiefwfjio

---
+++ Assets/DesactivarFueguecito.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesactivarFueguecito : MonoBehaviour 
+{
+	public GameObject fuego;
+
+	void OnTriggerExit(Collider col){
+		if (col.tag == "Player") {
+			fuego.SetActive (false);
+			gameObject.SetActive (false);
+		}
+	}
+}
